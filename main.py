c61aa9071ca4a1de4fd1b85771986b0074b1151d
pythonai250824 <pythonai250824@gmail.com>
2025-07-28 18:42:16 +0300

    upload

--- main.py
+++ main.py
@@ -4,5 +4,22 @@ import streamlit as st
 import psycopg2
 import psycopg2.extras
 
-psycopg2.connect(dbname='postgres', user='postgres', password='admin',
+def get_connection():
+    return psycopg2.connect(dbname='postgres', user='postgres', password='admin',
                  host='localhost', port='5432', cursor_factory=psycopg2.extras.RealDictCursor)
+
+try:
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+      CREATE TABLE if not exists users (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL
+            );
+    """)
+    conn.commit()
+    conn.close()
+except Exception as e:
+    print(f'==== Error: {e}')
+
