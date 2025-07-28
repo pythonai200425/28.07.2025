38439971ae77eb9f58cc1ff68a406e4d2f1de08b
pythonai250824 <pythonai250824@gmail.com>
2025-07-28 18:56:40 +0300

    upload

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ def get_connection():
     return psycopg2.connect(dbname='postgres', user='postgres', password='admin',
                  host='localhost', port='5432', cursor_factory=psycopg2.extras.RealDictCursor)
 
-try:
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("""
-      CREATE TABLE if not exists users (
-                id SERIAL PRIMARY KEY,
-                name TEXT NOT NULL,
-                email TEXT UNIQUE NOT NULL
-            );
-    """)
-    conn.commit()
-    conn.close()
-except Exception as e:
-    print(f'==== Error: {e}')
+# try:
+#     conn = get_connection()
+#     cur = conn.cursor()
+#     cur.execute("""
+#       CREATE TABLE if not exists users (
+#                 id SERIAL PRIMARY KEY,
+#                 name TEXT NOT NULL,
+#                 email TEXT UNIQUE NOT NULL
+#             );
+#     """)
+#     conn.commit()
+#     conn.close()
+# except Exception as e:
+#     print(f'==== Error: {e}')
 
+# run from Terminal
+# streamlit run main.py
+st.title("🚀 User Management with PostgreSQL")
