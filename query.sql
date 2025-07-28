c19553a98544ea66ad8c086484c6ce34131dd0b6
pythonai250824 <pythonai250824@gmail.com>
2025-07-28 18:00:57 +0300

    upload

--- query.sql
+++ query.sql
@@ -3,3 +3,14 @@
             name TEXT NOT NULL,
             email TEXT UNIQUE NOT NULL
         );
+
+ INSERT INTO users (name, email) values
+ ('itay', 'itay@gmail.com'),
+ ('rina', 'rina@walla.co.il');
+
+ UPDATE users
+ set name='rina', email='rina@walla.com'
+ where name='rina';
+
+ DELETE from users
+ where id = 3;
