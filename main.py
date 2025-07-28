b3954db9435a8b31cad8c74a574ef976fc58fb09
pythonai250824 <pythonai250824@gmail.com>
2025-07-28 18:33:11 +0300

    upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
+# pip install streamlit psycopg2-binary
 import streamlit as st
 import psycopg2
 import psycopg2.extras
 
-# pip install
+psycopg2.connect(dbname='postgres', user='postgres', password='admin',
+                 host='localhost', port='5432', cursor_factory=psycopg2.extras.RealDictCursor)
